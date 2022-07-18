dd92982fad392d58c3e5d84b619c659975a27138
loic <loic.gondet@hotmail.fr>
2022-07-18 22:27:39 +0200

    last modif

--- Assets/Scripts/MenuScene/MainManager.cs
+++ Assets/Scripts/MenuScene/MainManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
+    public static MainManager Instance { get; private set; }
     public Color TeamColor;
 
     private void Awake()
@@ -47,33 +47,3 @@ public class MainManager : MonoBehaviour
         }
     }
 }
-
-
-
-//[System.Serializable]
-//public class SaveData
-//{
-//    public Color TeamColor;
-
-//    public void SaveColor()
-//    {
-//        SaveData data = new SaveData();
-//        data.TeamColor = TeamColor;
-
-//        string json = JsonUtility.ToJson(data);
-
-//        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
-//    }
-
-//    public void LoadColor()
-//    {
-//        string path = Application.persistentDataPath + "savefile.json";
-//        if (File.Exists(path))
-//        {
-//            string json = File.ReadAllText(path);
-//            SaveData data = JsonUtility.FromJson<SaveData>(json);
-
-//            TeamColor = data.TeamColor;
-//        }
-//    }
-//}
--- Assets/Scripts/ResourcePile.cs
+++ Assets/Scripts/ResourcePile.cs
@@ -8,10 +8,24 @@ using UnityEngine;
 /// </summary>
 public class ResourcePile : Building
 {
+    private float m_ProductionSpeed = 0.5f;
+    public float ProductionSpeed
+    {
+        get { return m_ProductionSpeed; } // getter returns backing field
+        set {
+            if (value < 0.0f)
+            {
+
+                Debug.LogError("You can't set a negative production speed!");
+            }
+            else
+            {
+                m_ProductionSpeed = value; // original setter now in if/else statement
+            }
+        } // setter uses backing field
+    }
+
     public ResourceItem Item;
-
-    public float ProductionSpeed = 0.5f;
-
     private float m_CurrentProduction = 0.0f;
 
     private void Update()
@@ -21,7 +35,7 @@ public class ResourcePile : Building
             int amountToAdd = Mathf.FloorToInt(m_CurrentProduction);
             int leftOver = AddItem(Item.Id, amountToAdd);
 
-            m_CurrentProduction = m_CurrentProduction - amountToAdd + leftOver;
+            m_CurrentProduction = ProductionSpeed - amountToAdd + leftOver;
         }
         
         if (m_CurrentProduction < 1.0f)
@@ -32,7 +46,7 @@ public class ResourcePile : Building
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
+        return $"Producing at the speed of {m_ProductionSpeed}/s";
         
     }
     
